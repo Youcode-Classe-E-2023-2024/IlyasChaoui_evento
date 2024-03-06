3f25191224bded843de6606abb0929978ec75ce4
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-06 16:05:08 +0100

    add all route nedded

--- IlyasChaoui_evento/routes/web.php
+++ IlyasChaoui_evento/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\ForgetPasswordController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homePage');
+});
+
+Route::post('/register', [UserController::class , 'store'])->name('store.user');
+Route::get('/register', [UserController::class , 'index']);
+Route::post('/login', [UserController::class , 'login'])->name('login.trait');
+Route::post('/sendMail', [ForgetPasswordController::class , 'sendMail'])->name('forgetPassword.trait');
+Route::get('/forgetPassword', [ForgetPasswordController::class , 'showForgetPasswordForm']);
+Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+
+
+
+
+
+
+Route::get('/login', function () {
+    return view('authentication.authentication');
 });
