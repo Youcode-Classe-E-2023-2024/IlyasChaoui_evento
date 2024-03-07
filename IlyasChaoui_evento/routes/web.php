a82ba1c7919e716fc64272f174db23476f742e03
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-07 12:57:05 +0100

    add event route and logout added

--- IlyasChaoui_evento/routes/web.php
+++ IlyasChaoui_evento/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ForgetPasswordController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homePage');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::post('/register', [UserController::class , 'store'])->name('store.user');
 Route::get('/register', [UserController::class , 'index']);
@@ -27,6 +27,8 @@ Route::post('/sendMail', [ForgetPasswordController::class , 'sendMail'])->name('
 Route::get('/forgetPassword', [ForgetPasswordController::class , 'showForgetPasswordForm']);
 Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::post('/add/event', [EventController::class, 'store'])->name('add.event');
 
 
 
