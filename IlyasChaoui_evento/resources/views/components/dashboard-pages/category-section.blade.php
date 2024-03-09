170b20329462ad9b6f69cf5f08f4442d04d22636
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-09 10:55:08 +0100

    add category section component
    Co-Authored-By: Dmitry Dobrynin <dd@tovarisch.engineer>

---
+++ IlyasChaoui_evento/resources/views/components/dashboard-pages/category-section.blade.php
@@ -0,0 +1,14 @@
+<main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+    <div class="space-y-2 text-center">
+        <h2 class="text-3xl mt-20 text-white dark:text-dark font-bold">All Categories</h2>
+        <p class="font-serif text-sm text-gray-600 dark:text-gray-400">Here you can add and see all rss.</p>
+    </div>
+    <div class="w-full px-6 py-6 mx-auto">
+        <div class="flex  flex-col justify-between lg:flex-row">
+
+            <x-forms.category-form />
+
+            <x-tables.category-table :data="$data" />
+        </div>
+    </div>
+</main>
