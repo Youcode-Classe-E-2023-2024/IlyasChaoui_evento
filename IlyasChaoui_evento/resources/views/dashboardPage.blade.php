cc0833d25498245cfb45dec6c6b6f265bdf051cb
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-09 10:58:03 +0100

    Update dashboardPage.blade.php
    Co-Authored-By: Dmitry Dobrynin <dd@tovarisch.engineer>

--- IlyasChaoui_evento/resources/views/dashboardPage.blade.php
+++ IlyasChaoui_evento/resources/views/dashboardPage.blade.php
@@ -1,5 +1,20 @@
 @extends('Layouts.dashboardLayout')
-
 @section('main')
-    <x-dashboard-pages.static-section />
+    @if (Request::url() === 'http://127.0.0.1:8000/category')
+        @section('title')
+            Category
+        @endsection
+        <x-dashboard-pages.category-section :data="$data" />
+    @elseif(Request::url() === 'http://127.0.0.1:8000/static')
+        @section('title')
+            Static
+        @endsection
+        <x-dashboard-pages.static-section :data="$data" />
+    @else
+        @section('title')
+            Event
+        @endsection
+        <x-dashboard-pages.event-section :data="$data"  />
+    @endif
+
 @endsection
