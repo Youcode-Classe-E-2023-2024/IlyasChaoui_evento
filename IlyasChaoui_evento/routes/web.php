4aedd8a97b540aa373b868f30a75c4616b2ca0cd
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-09 11:01:06 +0100

    add all route nedded
    Co-Authored-By: Dmitry Dobrynin <dd@tovarisch.engineer>

--- IlyasChaoui_evento/routes/web.php
+++ IlyasChaoui_evento/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ForgetPasswordController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UserController;
+use App\Models\Category;
+use App\Models\Event;
+use App\Models\Reservation;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,6 +36,20 @@ Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name(
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 Route::post('/add/event', [EventController::class, 'store'])->name('add.event');
 Route::get('/static', [DashboardController::class, 'index'])->name('static.dashboard');
+Route::get('/content/{id}/{categoryId}', [HomeController::class, 'showContentPage'])->name('content.page');
+
+Route::get('/category', [CategoryController::class, 'index'])->name('category.dashboard');
+Route::post('/category/add', [CategoryController::class, 'store'])->name('add.category');
+Route::delete('/delete-category/{id}', [CategoryController::class, 'deleteCategory'])->name('delete.category');
+Route::put('/update-category/{id}', [CategoryController::class, 'updateCategory'])->name('update.category');
+
+Route::get('/event', [EventController::class, 'showEvents'])->name('show.event.dashboard');
+Route::post('/event/approve/{id}', [EventController::class, 'approveEvent'])->name('approve.event');
+Route::post('/event/decline/{id}', [EventController::class, 'declineEvent'])->name('decline.event');
+Route::post('/reservation', [ReservationController::class, 'reserve'])->name('reserve.ticket');
+Route::get('/dashboard/changeRole', [DashboardController::class, 'changeRole'])->name('change.role');
+Route::post('/dashboard/update', [DashboardController::class, 'updateRole'])->name('update.role');
+
 
 Route::get('/login', function () {
     return view('authentication.authentication');
