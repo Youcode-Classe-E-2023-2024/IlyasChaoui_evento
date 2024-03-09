51043bf720449118b4a5cadb14385232df379212
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-09 01:29:16 +0100

    add relation to reservation model

---
+++ IlyasChaoui_evento/app/Models/Reservation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+    protected $table = 'reservation';
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'status',
+        'email'
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id');
+    }
+}
